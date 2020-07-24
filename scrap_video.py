f761ec8c04f66acef85446d15a2af91bd48ca71a
Rohit Kaushik <kaushikrohit325@gmail.com>
2020-07-24 12:20:43 +0200

    add script to scrap video

---
+++ scrap_video.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import os
+import subprocess
+
+
+def scrap_videos():
+    path = './data/BBC/54/2015/no_transcripts'
+    sources = os.listdir(path)
+
+    for source in sources:
+        source_path = os.path.join(path, source)
+        source_df = pd.read_csv(source_path, index_col=0)
+
+        for index, row in source_df.iterrows():
+            source_name = row['Source']
+            program = row['Program Name']
+            date = row['Date']
+            reason = row['Unavailable reason']
+
+            if 'has to be requested' in reason or 'Problem' in reason:
+                print('skipping {} {} for {} because {}'.format(row['Source'], row['Program Name'], row['Date'],
+                                                                reason))
+                continue
+
+            output_template = '-o ./videos/{}-{}-{}'.format(source_name, program, date)
+            video_link = row['video_link']
+            print('getting video from {}'.format(video_link))
+            cmd = ['youtube-dl', output_template , video_link]
+            proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+            o, e = proc.communicate()
+
+            print('Error: ' + e.decode('ascii'))
+
+
+if __name__ == '__main__':
+    scrap_videos()
